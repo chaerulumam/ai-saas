6a96654ae7a6103e92c411973442d07698ce9e08
Chaerul Umam <chaerulumamb244@gmail.com>
2023-08-06 14:57:57 +0700

    dashboard page

---
+++ components/sidebar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+export const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill src="/logo1.png" alt="Logo" />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            UniqueAI
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
