eff5004329c443f2947d0e9b2ce89e888df78414
Chaerul Umam <chaerulumamb244@gmail.com>
2023-08-06 13:52:36 +0700

    modify landing navbar

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const font = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -11,13 +12,20 @@ export const LandingNavbar = () => {
   return (
     <div className="p-4 transparent flex items-center justify-between">
       <Link href="/" className="flex items-center">
-        <div className="relative h-16 w-16 mr-0">
-          <Image src="/logo.png" alt="logo" fill />
+        <div className="relative h-8 w-8 mr-4">
+          <Image src="/logo1.png" alt="logo" fill />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-          Unique
+          UniqueAI
         </h1>
       </Link>
+      <div className="flex items-center justify-end">
+        <Link href="/">
+          <Button className="rounded-full" variant="premium">
+            Get Started
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
