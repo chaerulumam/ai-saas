294a6744b167d4f43422f28e5a5d57efa4b5932a
Chaerul Umam <chaerulumamb244@gmail.com>
2023-08-06 20:05:22 +0700

    sidebar content

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,16 +2,58 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
+import {
+  Code,
+  ImageIcon,
+  MessageSquare,
+  PencilLine,
+  VideoIcon,
+} from "lucide-react";
 
 const montserrat = Montserrat({
   weight: "600",
   subsets: ["latin"],
 });
 
+const routes = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-700",
+    href: "/conversation",
+  },
+  {
+    label: "Grammar Corrector",
+    icon: PencilLine,
+    color: "text-green-500",
+    href: "/grammar",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    href: "/code",
+  },
+];
+
 export const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -23,6 +65,25 @@ export const Sidebar = () => {
             UniqueAI
           </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
